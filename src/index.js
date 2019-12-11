4f3696a80c03e83bdbd37bb0d95dde7cda8761fb
Andrew MacLeod <A6macleod@gmail.com>
2019-12-10 21:54:21 -0500

    click selector functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 const imageSelector = document.querySelectorAll('.image-selector');
+const mainContainer = document.querySelector('#main-container');
+const viewer = document.querySelector('#wide-image-container');
+
+function updateImage (selector) {
+  const orientation = selector.getAttribute('data-orientation');
+
+  // clear all classes from the image container and the viewer
+  mainContainer.className = '';
+  viewer.className = '';
+  
+  if (orientation == 'landscape') {
+    mainContainer.classList.add('main-container-landscape');
+  } else {
+    mainContainer.classList.add('main-container-portrait');
+  }
+  viewer.classList.add(`image-view-${selector.getAttribute('data-number')}`);
+}
 
 function selectAnImage () {
   imageSelector.forEach(selector => selector.classList.remove('selected-image-selector'));
   this.classList.add('selected-image-selector');
+
+  updateImage(this);
 }
 
 imageSelector.forEach(selector => selector.addEventListener('click', selectAnImage));
