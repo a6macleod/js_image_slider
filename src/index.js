d793d123fe4bfaf80d87706ecbaa2602d0b1e921
Andrew MacLeod <A6macleod@gmail.com>
2019-12-11 22:22:16 -0500

    add timed image change

--- src/index.js
+++ src/index.js
@@ -1,27 +1,63 @@
-const imageSelector = document.querySelectorAll('.image-selector');
-const mainContainer = document.querySelector('#main-container');
-const viewer = document.querySelector('#wide-image-container');
+const imageSelector = document.querySelectorAll(".image-selector");
+const mainContainer = document.querySelector("#main-container");
+const viewer = document.querySelector("#wide-image-container");
+let imageIntevalTimer = setInterval(nextImage, 5000);
 
-function updateImage (selector) {
-  const orientation = selector.getAttribute('data-orientation');
+function updateImage(selector) {
+  const orientation = selector.getAttribute("data-orientation");
 
   // clear all classes from the image container and the viewer
-  mainContainer.className = '';
-  viewer.className = '';
-  
-  if (orientation == 'landscape') {
-    mainContainer.classList.add('main-container-landscape');
+  mainContainer.className = "";
+  viewer.className = "";
+
+  if (orientation == "landscape") {
+    mainContainer.classList.add("main-container-landscape");
   } else {
-    mainContainer.classList.add('main-container-portrait');
+    mainContainer.classList.add("main-container-portrait");
   }
-  viewer.classList.add(`image-view-${selector.getAttribute('data-number')}`);
+  viewer.classList.add(`image-view-${selector.getAttribute("data-number")}`);
 }
 
-function selectAnImage () {
-  imageSelector.forEach(selector => selector.classList.remove('selected-image-selector'));
-  this.classList.add('selected-image-selector');
+function nextImage() {
+  const currentSlide = document.querySelector(".selected-image-selector");
+  let nextImageNumber = parseInt(currentSlide.getAttribute("data-number")) + 1;
+  if (nextImageNumber > imageSelector.length) {
+    nextImageNumber = 1;
+  }
 
-  updateImage(this);
+  const nextSlide = document.querySelector(
+    `.image-selector-${nextImageNumber}`
+  );
+  selectAnImage(nextSlide);
 }
 
-imageSelector.forEach(selector => selector.addEventListener('click', selectAnImage));
+function selectAnImage(selectedImage) {
+  imageSelector.forEach(selector =>
+    selector.classList.remove("selected-image-selector")
+  );
+  selectedImage.classList.add("selected-image-selector");
+
+  updateImage(selectedImage);
+}
+
+function clickSelector() {
+  selectAnImage(this);
+}
+
+function startTimer() {
+  imageIntevalTimer = setInterval(nextImage, 5000);
+}
+
+function endTimer() {
+  clearInterval(imageIntevalTimer);
+}
+
+imageSelector.forEach(selector =>
+  selector.addEventListener("click", clickSelector)
+);
+imageSelector.forEach(selector =>
+  selector.addEventListener("mouseenter", endTimer)
+);
+imageSelector.forEach(selector =>
+  selector.addEventListener("mouseout", startTimer)
+);
